0e9f806e065d9665c99f0f60e3f98f65035eaa1a
filipe11402 <filipe11402@outlook.pt>
2023-04-03 22:06:53 +0100

    added class that represents needed CSV to read

---
+++ src/Locations.Consoles/CsvLocation.cs
@@ -0,0 +1,18 @@
+﻿namespace Locations.Consoles;
+
+public sealed class CsvLocation
+{
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public int ZeroValue { get; set; }
+
+    public double Altitude { get; set; }
+
+    public double NumberOfDays { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Time { get; set; }
+}
--- src/Locations.Consoles/Program.cs
+++ src/Locations.Consoles/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using CsvHelper;
+using System.Globalization;
+
+var fileLocation = $@"{Directory.GetCurrentDirectory()}\..\..\..\..\..\assets\20081026094426.csv.csv";
+
+using var streamReader = new StreamReader(Path.GetFullPath(fileLocation));
+
+using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+
+csvReader.Dispose();
+streamReader.Dispose();
