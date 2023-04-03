8bb26ab4bc26a6754801e952a6f2415cbbdd17d9
filipe11402 <filipe11402@outlook.pt>
2023-04-03 22:07:30 +0100

    added initial CTOR to `CsvLocation`

--- src/Locations.Consoles/CsvLocation.cs
+++ src/Locations.Consoles/CsvLocation.cs
@@ -2,6 +2,7 @@
 
 public sealed class CsvLocation
 {
+    //TODO: encapsulation
     public double Latitude { get; set; }
 
     public double Longitude { get; set; }
@@ -15,4 +16,22 @@ public sealed class CsvLocation
     public DateTime Date { get; set; }
 
     public string Time { get; set; }
+
+    public CsvLocation(
+        double latitude,
+        double longitude, 
+        int zeroValue, 
+        double altitude, 
+        double numberOfDays,
+        DateTime date, 
+        string time)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+        ZeroValue = zeroValue;
+        Altitude = altitude;
+        NumberOfDays = numberOfDays;
+        Date = date;
+        Time = time;
+    }
 }
