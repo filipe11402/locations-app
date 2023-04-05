e99dd9b7dcb45214d99a35623182d986f57d0aaf
Filipe Encarnação <filipe.encarnacao@connectlime.com>
2023-04-05 22:23:26 +0100

    added logic to show missing data as in time differece, distance traveled, speed in meters per second

--- src/Locations.Consoles/Program.cs
+++ src/Locations.Consoles/Program.cs
@@ -28,12 +28,26 @@ for (int totalLocation = 0; totalLocation < locations.Count; totalLocation++)
     var startLocation = locations[totalLocation];
     var endLocation = locations[totalLocation + 1];
 
-    Console.WriteLine($"PONTO {locationPoint} VS PONTO {locationPoint + 1}");
+    var startLocationCount = 1;
+    var endLocationCount = startLocationCount++;
+
+    Console.WriteLine($"PONTO {startLocationCount} VS PONTO {endLocationCount}");
 
     var initialTime = DateTime.Parse($"{startLocation.Date.ToString("MM/dd/yyyy")} {startLocation.Time}");
     var endTime = DateTime.Parse($"{endLocation.Date.ToString("MM/dd/yyyy")} {endLocation.Time}");
+    var timeDifferece = initialTime.CalculateTimeDifferenceInSeconds(endTime);
 
-    Console.WriteLine($"TEMPO DECORRIDO: {initialTime.CalculateTimeDifferenceInSeconds(endTime)}");
+    Console.WriteLine($"TEMPO DECORRIDO: {timeDifferece}s");
+
+    var traveledDistance = LocationHelpers.CalculateTraveledDistanceInSeconds(startLocation, endLocation);
+
+    Console.WriteLine($"DISTANCIA ENTRE PONTO {startLocationCount} VS PONTO {endLocationCount}: {traveledDistance}m");
+
+    var transportationSpeed = traveledDistance.CalculateSpeedInMetersPerSecond(timeDifferece);
+
+    Console.WriteLine($"VELOCIDADE ENTRE PONTO {startLocationCount} VS PONTO {endLocationCount}: {transportationSpeed} m/s");
+
+    Task.Delay(2000);
 
     locationPoint++;
 }
